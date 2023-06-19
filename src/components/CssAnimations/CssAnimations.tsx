8eccea7c49fe88d232c74adae9a5c01dbeba5647
bast44trl <b.traille@hotmail.fr>
2023-06-19 16:15:41 +0200

    added rotate scale and skew animations

--- src/components/CssAnimations/CssAnimations.tsx
+++ src/components/CssAnimations/CssAnimations.tsx
@@ -15,16 +15,41 @@ function CssAnimations() {
         </div>
       </div>
       <div className="container">
-        <div className="btn__blow">
+        <div className="btn blow">
           <span>Gonfle moi !</span>
           <span id="mtn">(Maintenir)</span>
         </div>
         <div className="ball"></div>
       </div>
       <div className="container">
-        <button className="btn__boop">Transform!</button>
+        <button className="btn boop">Transform!</button>
         <div className="box">
           <span>Boop!</span>
+        </div>
+      </div>
+      <div className="container">
+        <button className="btn rot">Rotate!</button>
+        <div className="boxes">
+          <div className="boxes__base boxes--rotDegrees">rotate (360deg)</div>
+          <div className="boxes__base boxes--rotTurns">rotate (1turn)</div>
+        </div>
+      </div>
+      <div className="container">
+        <button className="btn combine">Rotate & transform!</button>
+        <div>
+          <span>Boop!</span>
+        </div>
+      </div>
+      <div className="container">
+        <button className="btn skew">Skew (40deg)</button>
+        <div>
+          <span>Skew</span>
+        </div>
+        <div>
+          <span>SkewY</span>
+        </div>
+        <div>
+          <span>SkewX</span>
         </div>
       </div>
     </div>
